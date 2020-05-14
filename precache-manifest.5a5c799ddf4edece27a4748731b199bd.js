20fadce9a8bf1299ce59584361f096cd67ac93c1
UNL-Gironde <65370492+UNL-Gironde@users.noreply.github.com>
2020-05-15 00:49:00 +0200

    Updates

---
+++ precache-manifest.5a5c799ddf4edece27a4748731b199bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebd1fec8a71b5df15d0aa768eeefa9b",
+    "url": "/Calculateur-de-moyenne/index.html"
+  },
+  {
+    "revision": "0cd82c4764c0fa372a56",
+    "url": "/Calculateur-de-moyenne/static/css/main.33e5d89c.chunk.css"
+  },
+  {
+    "revision": "9c1dcd096cc06a8df169",
+    "url": "/Calculateur-de-moyenne/static/js/2.6d4c2639.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calculateur-de-moyenne/static/js/2.6d4c2639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd82c4764c0fa372a56",
+    "url": "/Calculateur-de-moyenne/static/js/main.53e15e66.chunk.js"
+  },
+  {
+    "revision": "530863ca7144cb3f3ad7",
+    "url": "/Calculateur-de-moyenne/static/js/runtime-main.77e42ba2.js"
+  }
+]);
